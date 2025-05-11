163c702d26c1445a05c871c206a9fa1e41958b5b
engineer-dilnawaz <155418947+engineer-dilnawaz@users.noreply.github.com>
2025-05-11 14:21:27 +0500

    Add HTTP package and implement data fetching for grocery items
    - Integrated HTTP package for network requests.
    - Implemented loading and error handling for grocery items in GroceryList.
    - Added functionality to delete grocery items with server synchronization.
    - Enhanced NewItem to handle item creation with server response.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_groceries_app/data/categories.dart';
 import 'package:flutter_groceries_app/models/grocery_item.dart';
 import 'package:flutter_groceries_app/widgets/grocery_item.dart';
 import 'package:flutter_groceries_app/widgets/new_item.dart';
@@ -11,12 +15,66 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  bool _isLoading = true;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+      'flutter-app-max-default-rtdb.asia-southeast1.firebasedatabase.app',
+      'shpping-list.json',
+    );
+    final response = await http.get(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later';
+      });
+      return;
+    }
+
+    if (response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category =
+          categories.entries
+              .firstWhere(
+                (element) => element.value.title == item.value['category'],
+              )
+              .value;
+      loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    setState(() {
+      _groceryItems = loadedItems;
+      _isLoading = false;
+    });
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(
       context,
     ).push<GroceryItem>(MaterialPageRoute(builder: (ctx) => NewItem()));
+    // _loadItems();
+
     if (newItem == null) {
       return;
     }
@@ -26,15 +84,43 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(item) {
+  void _removeItem(item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+    final url = Uri.https(
+      'flutter-app-max-default-rtdb.asia-southeast1.firebasedatabase.app',
+      'shpping-list/${item.id}.json',
+    );
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+      if (!mounted) {
+        return;
+      }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          showCloseIcon: true,
+          content: Text(
+            '${item.name} couldn\'t be deleted. Please try again later!',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+          ),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = Center(child: Text('No items added yet.'));
+
+    if (_isLoading) {
+      content = Center(child: CircularProgressIndicator());
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
@@ -53,6 +139,10 @@ class _GroceryListState extends State<GroceryList> {
           );
         },
       );
+    }
+
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
 
     return Scaffold(
